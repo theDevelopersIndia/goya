6b0d67645a7b5419ec8ab5ecd4f1779098787a81
Rahul Kumar <dr.rahulk95@gmail.com>
2024-02-23 00:47:17 +0530

    updated video

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import React, { useRef } from "react";
 
 export default function Home() {
+  const videoRef = useRef(null);
+  const [canAutoplay, setCanAutoplay] = React.useState(false);
+  React.useEffect(() => {
+    const video = document.getElementById("videoPlayer");
+    if (video) {
+      video
+        .play()
+        .then(() => {})
+        .catch((error) => {
+          // Autoplay was prevented, handle the error here
+          console.error("Autoplay was prevented:", error.message);
+        });
+    }
+  }, [canAutoplay]);
+  const handlePlay = () => {
+    setCanAutoplay(true);
+  };
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Goya</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <video
+          id="videoPlayer"
+          ref={videoRef}
+          style={{ maxWidth: "100vw", maxHeight: "100vh" }}
+          autoPlay={canAutoplay}
+          loop
+          playsInline
+          onClick={handlePlay}
+        >
+          <source src="/GoyaLanding.mp4" type="video/mp4" />
+        </video>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
-  )
+  );
 }
